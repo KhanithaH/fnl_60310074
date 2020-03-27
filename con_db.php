1f1f5a6ee5cb44d72eb00d532cf1e0dff81af004
Khanitha Hnaebnak <60310074@go.buu.ac.th>
2020-03-27 15:25:17 +0700

    edit2

--- con_db.php
+++ con_db.php
@@ -1,5 +1,5 @@
 <?php
-$con= mysqli_connect("localhost","root","","login") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("localhost","root","","fnl_nodejs") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
